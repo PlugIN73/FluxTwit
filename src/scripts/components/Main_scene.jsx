d34f3314800c989a421212fbcca0098d76cff8e3
plugin73 <plugin73@undev.ru>
2014-09-29 05:18:29 +0000

    hastag and user search

--- src/scripts/components/Main_scene.jsx
+++ src/scripts/components/Main_scene.jsx
@@ -9,11 +9,20 @@ var React = require('react/addons');
 var Twit = require('./Twit.jsx');
 var TwitActions = require('../actions/TwitActions');
 var OAuth = require('oauth');
-var ENTER_KEY_CODE = 13;
+var oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/requesttoken',
+  'https://api.twitter.com/oauth/accesstoken',
+  'pFtPOLUXBXDtPHokigyXN9UAT',
+  'FlGNi9fhU2q3Ucfi5M9czBraY3QxEkPflLntLJgrBjA0ecMpon',
+  '1.0A',
+  null,
+  'HMAC-SHA1'
+);
+var ENTERKEY_CODE = 13;
 
 require('../../styles/Main_scene.css');
 
-var Main_scene = React.createClass({
+var MainScene = React.createClass({
 
   getInitialState: function() {
     return {
@@ -28,46 +37,49 @@ var Main_scene = React.createClass({
     }
     return (
       <section id="main">
-        <input key="input" onBlur={this._save} onChange={this._onChange} onKeyDown={this._onKeyDown} value={this.state.value} autoFocus={true} />
+        <input key="input" onChange={this.onChange} onKeyDown={this.onKeyDown} value={this.state.value} autoFocus={true} />
         <ul id="todo-list">{twits}</ul>
       </section>
     );
   },
 
-  _save: function() {
+  onEnter: function() {
     TwitActions.clearAll();
-    var text = this.state.value;
-    if (text.trim()){
-      this._getTwits(text);
+    var text = this.state.value.trim();
+    if (text[0] === "@") {
+      this.getUserTimeline(text.substr(1));
+    } else if (text[0] === "#") {
+      this.getTwits(text.substr(1));
     }
   },
 
-  _onChange: function(event) {
+  onChange: function(event) {
     this.setState({
       value: event.target.value
     });
   },
 
-  _onKeyDown: function(event) {
-    if (event.keyCode === ENTER_KEY_CODE) {
-      this._save();
+  onKeyDown: function(event) {
+    if (event.keyCode === ENTERKEY_CODE) {
+      this.onEnter();
     }
   },
 
-  _getTwits: function(user) {
-
-    var oauth = new OAuth.OAuth(
-      'https://api.twitter.com/oauth/request_token',
-      'https://api.twitter.com/oauth/access_token',
-      'pFtPOLUXBXDtPHokigyXN9UAT',
-      'FlGNi9fhU2q3Ucfi5M9czBraY3QxEkPflLntLJgrBjA0ecMpon',
-      '1.0A',
-      null,
-      'HMAC-SHA1'
-    );
-
+  getUserTimeline: function(user) {
     oauth.get(
-      'https://api.twitter.com/1.1/search/tweets.json?q=' + user + '&result_type=recent',
+      'https://api.twitter.com/1.1/statuses/usertimeline.json?screen_name=' + user,
+      '978792919-URT380OU0ofxqomr8hUPXOxgoJAMRqUODAGQEdoQ', //test user token
+      '4Gx37DpzwwVcBszGP51hUwegmuFnSsxUZ9IbuizUbbfMW', //test user secret
+      function (e, data, res){
+        if (e) console.error(e);
+        var twits = JSON.parse(data);
+        TwitActions.create(twits);
+      });
+  },
+
+  getTwits: function(hashtag) {
+    oauth.get(
+      'https://api.twitter.com/1.1/search/tweets.json?q=' + hashtag + '&resulttype=recent',
       '978792919-URT380OU0ofxqomr8hUPXOxgoJAMRqUODAGQEdoQ', //test user token
       '4Gx37DpzwwVcBszGP51hUwegmuFnSsxUZ9IbuizUbbfMW', //test user secret
       function (e, data, res){
@@ -78,4 +90,4 @@ var Main_scene = React.createClass({
   }
 });
 
-module.exports = Main_scene;
+module.exports = MainScene;
